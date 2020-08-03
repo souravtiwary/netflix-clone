cd700c43b1f0cb10db1299005826ca290b2ace72
souravtiwary <59500703+souravtiwary@users.noreply.github.com>
2020-08-04 00:55:36 +0530

    final build

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://image.tmdb.org/t/p/w200/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 const base_url = "https://image.tmdb.org/t/p/w500";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   //state a short term memory every time the page refresh it will reload
   // empty movie array
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on the specific condition
   // when the page is loading it will do its work
@@ -23,6 +27,32 @@ function Row({ title, fetchUrl, isLargeRow }) {
   // if fetchUrl changes the useEffect will refresh
   console.log(movies);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          //https://www.youtube.com/watch?v=XtMThy8QKqU&t=116s
+          setTrailerUrl(urlParams.get("v")); //returns XtMThy8QKqU value of v
+        })
+        .catch((error) => {
+          console.log(error);
+          window.alert(error);
+        });
+    }
+  };
+
   return (
     <div className="row">
       {/* title */}
@@ -34,6 +64,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -42,6 +73,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
